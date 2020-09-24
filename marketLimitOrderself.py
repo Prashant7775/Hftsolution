ca5d4640a65becb63b084ffe4cb5bd8a1c8fad21
Prashant Singh <prashant12345678910@gmail.com>
2020-09-25 00:51:15 +0530

    Add files via upload

---
+++ marketLimitOrderself.py
@@ -0,0 +1,192 @@
+from datetime import datetime
+
+
+askSize={}
+bidSize={}
+
+class sellPrice:
+    def __init__(self):
+        self.data=set()
+        self.size=0
+    def push(self,e,shares):
+        e=float(e)
+        if self.size==0:
+            self.data.add(e)
+            self.size+=1
+            askSize[e]=shares
+        else:
+            if e in self.data:
+                askSize[e]+=shares
+            else:
+                self.data.add(e)
+                self.size+=1
+                askSize[e]=shares
+    def pop(self,e,shares):
+        e=float(e)
+        if e in self.data:
+            if shares>=askSize[e]:
+                askSize[e]=0
+                self.data.discard(e)
+                self.size-=1
+            else:
+                askSize[e]-=shares
+
+class buyPrice:
+    def __init__(self):
+        self.data=set()
+        self.size=0
+    def push(self,e,shares):
+        e=float(e)
+        if self.size==0:
+            self.data.add(e)
+            self.size+=1
+            bidSize[e]=shares
+        else:
+            if e in self.data:
+                bidSize[e]+=shares
+            else:
+                self.data.add(e)
+                self.size+=1
+                bidSize[e]=shares
+    def pop(self,e,shares):
+        e=float(e)
+        if e in self.data:
+            if shares>=bidSize[e]:
+                bidSize[e]=0
+                self.data.discard(e)
+                self.size-=1
+            else:
+                bidSize[e]-=shares
+                    
+sp=sellPrice()
+bp=buyPrice()
+
+while True:
+    f=open('log.txt','a')
+    print('Price\tAsk Size')
+    for i in sorted(sp.data,reverse=True)[-5:]:
+        print(str(i)+'\t'+str(askSize[i]))
+    print('-'*25)
+    print('Price\tBid Size')
+    for i in sorted(bp.data,reverse=True)[:5]:
+        print(str(i)+'\t'+str(bidSize[i]))
+    print('-'*25)
+    e=input('type "m" for market order and "l" for limit order')
+    if e=='m':
+        bORs = input('enter "b" to buy and "s" to sell')
+        if bORs not in 'bs':
+            print()
+            print('#'*20+'\nselect "b" or "s"\n'+'#'*20)
+            print()
+            continue
+        try:
+            shares=int(input("enter no. of shares"))
+        except:
+            print()
+            print('#'*20+'\nINVALID DATATYPE\n'+'#'*20)
+            print()
+            continue
+        if bORs == 'b':
+            if sp.size>0:
+                if askSize[min(sp.data)]>=shares:
+                    f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(shares)+' removed from ASK listing priced at '+str(min(sp.data))+'\n')
+                    sp.pop(min(sp.data),shares)
+                    shares=0
+                    
+                else:
+                    while sp.size>0 and shares>0:
+                        if askSize[min(sp.data)]>=shares:
+                            f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(shares)+' removed from ASK listing priced at '+str(min(sp.data))+'\n')
+                            sp.pop(min(sp.data),shares)
+                            shares=0
+                        else:
+                            f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(askSize[min(sp.data)])+' removed from ASK listing priced at '+str(min(sp.data))+'\n')
+                            shares-=askSize[min(sp.data)]
+                            sp.pop(min(sp.data),askSize[min(sp.data)])
+        if bORs=='s':
+            if bp.size>0:
+                if bidSize[max(bp.data)]>=shares:
+                    f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(shares)+' removed from BID listing priced at '+str(max(bp.data))+'\n')
+                    bp.pop(max(bp.data),shares)
+                    shares=0
+                else:
+                    while bp.size>0 and shares>0:
+                        if bidSize[max(bp.data)]>=shares:
+                            f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(shares)+' removed from BID listing priced at '+str(max(bp.data))+'\n')
+                            bp.pop(max(bp.data),shares)
+                            shares=0
+                        else:
+                            f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(bidSize[max(bp.data)])+' removed from BID listing priced at '+str(max(bp.data))+'\n')
+                            shares-=bidSize[max(bp.data)]
+                            bp.pop(max(bp.data),bidSize[max(bp.data)])
+                    
+        
+    elif e=='l':
+        bORs = input('enter "b" to buy and "s" to sell')
+        if bORs not in 'bs':
+            print()
+            print('#'*20+'\nselect "b" or "s"\n'+'#'*20)
+            print()
+            continue
+        try:
+            shares=int(input("enter no. of shares"))
+            price=float(input("enter price"))
+        except:
+            print()
+            print('#'*20+'\nINVALID DATATYPE\n'+'#'*20)
+            print()
+            continue
+        
+        if bORs == 'b':
+            if sp.size>0:
+                if askSize[min(sp.data)]>=shares and min(sp.data)<=price:
+                    f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(shares)+' removed from ASK listing priced at '+str(min(sp.data))+'\n')
+                    sp.pop(min(sp.data),shares)
+                    shares=0
+            flag=True
+            while sp.size>0 and shares>0 and flag:
+                if askSize[min(sp.data)]>=shares and min(sp.data)<=price:
+                    f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(shares)+' removed from ASK listing priced at '+str(min(sp.data))+'\n')
+                    sp.pop(min(sp.data),shares)
+                    shares=0
+                    break
+                if askSize[min(sp.data)]<shares and min(sp.data)<=price:
+                    temp=shares
+                    f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(askSize[min(sp.data)])+' removed from ASK listing priced at '+str(min(sp.data))+'\n')
+                    shares-=askSize[min(sp.data)]
+                    sp.pop(min(sp.data),temp)
+                    #print(sp.data,shares,min(sp.data))
+                elif min(sp.data)>price:
+                    flag=False
+                    #print(sp.data)
+                
+            else:
+                if shares>0:
+                    f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(shares)+' added to BID listing priced at '+str(price)+'\n')
+                    bp.push(price,shares)
+
+        elif bORs == 's':
+            if bp.size>0:
+                if bidSize[max(bp.data)]>=shares and max(bp.data)>=price:
+                    f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(shares)+' removed from BID listing priced at '+str(max(bp.data))+'\n')
+                    bp.pop(max(bp.data),shares)
+                    shares=0
+            flag=True
+            while bp.size>0 and shares>0 and flag:
+                if bidSize[max(bp.data)]>=shares and max(bp.data)>=price:
+                    f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(shares)+' removed from BID listing priced at '+str(max(bp.data))+'\n')
+                    bp.pop(max(bp.data),shares)
+                    shares=0
+                    break
+                if bidSize[max(bp.data)]<shares and max(bp.data)>=price:
+                    temp=shares
+                    f.write(str(time.time())+' : '+str(bidSize[max(bp.data)])+' removed from BID listing priced at '+str(max(bp.data))+'\n')
+                    shares-=bidSize[max(bp.data)]
+                    bp.pop(max(bp.data),temp)
+                elif max(bp.data)<price:
+                    flag=False
+            else:
+                if shares>0:
+                    f.write(str(datetime.now().strftime("%H:%M:%S"))+' - '+str(shares)+' added to ASK listing priced at '+str(price)+'\n')
+                    sp.push(price,shares)
+    f.close()
